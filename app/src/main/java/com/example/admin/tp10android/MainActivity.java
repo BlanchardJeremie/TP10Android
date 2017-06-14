b158ee2125cf8795cd6ce794a3ae7f3ecde4bb1a
Blanchard Jérémie <jeremie.blanchard@outlook.fr>
2017-06-14 10:59:48 +0200

    add interface

--- app/src/main/java/com/example/admin/tp10android/MainActivity.java
+++ app/src/main/java/com/example/admin/tp10android/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.admin.tp10android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView pile1;
+    TextView pile2;
+    TextView pile3;
+    TextView pile4;
+    TextView resultat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+
+
+
+
     }
 }
